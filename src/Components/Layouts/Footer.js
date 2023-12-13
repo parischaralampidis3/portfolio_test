7e0c4466175d9332c075a61480c1b31624ecd22c
@parou.char <curlycode@outlook.com>
2023-12-13 12:09:00 +0200

    make microfixes at the position. 0.2

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="w-full mt-12">
-      <div className="flex bg-red-50 mt-20 p-2 fixed bottom-0 w-full">
+      <div className="flex bg-red-50 mt-0 p-2 fixed bottom-0 w-full">
         <div className="container  flex flex-wrap justify-center mx-auto text-center md:w-50 text-sm lg:text-lg w-full ">
           <h1 className="self-end">Paris Charalampidis &copy; All rights reserved 2023</h1>
         </div>
