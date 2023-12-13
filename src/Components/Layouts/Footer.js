0c3cc2b4ad9060e9c098c2245133ad9dccdf83ac
@parou.char <curlycode@outlook.com>
2023-12-13 11:36:13 +0200

    make microfixes at the code. 0.3

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -2,17 +2,13 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="w-full">
-      <div className="bg-red-50 mt-20  p-12">
-        <div className="container flex flex-wrap justify-around  mx-auto text-center md:w-50 text-sm lg:text-lg w-full  ">
-  
-             <h1 className="">Paris Charalampidis &copy; All rights reserved 2023</h1>
-         
-          </div>
-         
+    <div className="w-full mt-12">
+      <div className="flex bg-red-50 mt-20 p-2 fixed bottom-0 w-full">
+        <div className="container  flex flex-wrap justify-center mx-auto text-center md:w-50 text-sm lg:text-lg w-full ">
+          <h1 className="self-end">Paris Charalampidis &copy; All rights reserved 2023</h1>
         </div>
       </div>
- 
+    </div>
   );
 }
 
